f0254937edc5667489803e2736f90a5f230fb04a
Rutika <rutikatharali@gmail.com>
2026-03-02 12:02:12 +0530

    model_train

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,88 +1,133 @@
 import sys
 from dataclasses import dataclass
-import numpy as np
+
+import numpy as np 
 import pandas as pd
-from sklearn.compose import ColumnTransformer #applying pipline to columns :[ (name, transformer, columns) ]
-from sklearn.impute import SimpleImputer #missing values handling :fill with median here
-from sklearn.pipeline import Pipeline #stey-by-step execution :steps[('name_of_step', transformer_object)]
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
 from src.exception import CustomException
 from src.logger import logging
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from src.utilis import save_object
 import os
 
+from src.utilis import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path:str=os.path.join('artifacts','preprocessor.pkl') 
+    #preprocessor's memory, saved permanently to disk.
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
+
     def get_data_transformer_object(self):
-        """
+        '''
         This function is responsible for data transformation
-        """
+        
+        '''
         try:
-            numerical_columns=['math score','reading score','writing score']
-            categoical_columns=['gender','race/ethnicity','parental level of education','lunch','test preparation course']
-            num_pipeline=Pipeline( 
+            numerical_columns = ["writing score", "reading score"]
+            categorical_columns = [
+                "gender",
+                "race/ethnicity",
+                "parental level of education",
+                "lunch",
+                "test preparation course",
+            ]
+
+            num_pipeline= Pipeline(
                 steps=[
-                    ('imputer',SimpleImputer(strategy='median')),
-                    ("Scaler",StandardScaler())
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
+
                 ]
             )
-            logging.info('Numerical columns imputation completed')
+
             cat_pipeline=Pipeline(
+
                 steps=[
-                    ('imputer',SimpleImputer(strategy='most_frequent')),
-                    ('one_hot_encoder',OneHotEncoder()),
-                    ('scaler',StandardScaler())
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
                 ]
+
             )
-            
-            logging.info('Categorical columns imputation completed')
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
             preprocessor=ColumnTransformer(
                 [
-                    ('num_pipeline',num_pipeline,numerical_columns),
-                    ('cat_pipeline',cat_pipeline,categoical_columns)
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
                 ]
-            )
+
+
+)
+
             return preprocessor
+        
         except Exception as e:
             raise CustomException(e,sys)
+        
     def initiate_data_transformation(self,train_path,test_path):
+
         try:
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
-            logging.info('Read train and test data completed')
-            logging.info("obtaning preprocessor object")
-            preproceesing_obj=self.get_data_transformer_object()
-            target_column_name='math score'
-            numerical_columns=['reading score','writing score']
-        
+
+            logging.info("Read train and test data completed")
+
+            logging.info("Obtaining preprocessing object")
+
+            preprocessing_obj=self.get_data_transformer_object()
+
+            target_column_name="math score"
+            #train data separated into input and target features to apply fit_transform()
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
+            #test data separated into input and target features to apply transform
+
             input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
             target_feature_test_df=test_df[target_column_name]
-            logging.info("Applying preprocessing object on training and testing dataframes")
-            input_feature_train_arr=preproceesing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preproceesing_obj.transform(input_feature_test_df) 
-            logging.info("Preprocessing completed")
-            train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
-            test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)] 
-            logging.info("Saved preprocessing object")
-            save_object(
-                file_path=self.data_transformation_config.preprocessor_obj_file_path,
-                obj=preproceesing_obj
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
             )
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            #np.c_ to concatenate the input features & target feature arrays horizontally to create the final train 
+            #and test arrays that can be used for model training and evaluation. 
+            #resulting train_arr & test_arr will have the input features & target variable combined in a single array,for ml algo.
+
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
+
+            #to apply this 
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                #preprocessing_obj = self.get_data_transformer_object() 
+                obj=preprocessing_obj
+
+            )
+
             return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
         except Exception as e:
             raise CustomException(e,sys)
-
-
-
