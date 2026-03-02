e2ab861ec21e04d62fe44d3726fd0b6237125de0
Rutika <rutikatharali@gmail.com>
2026-03-02 21:57:07 +0530

    add gunicorn

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         return render_template('home.html',results=results[0])
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
