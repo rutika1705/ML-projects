4c46915d4fb371ab61e408b7db3a7920b485680e
rutika1705 <rutikatharali@gmail.com>
2026-03-07 11:26:10 +0530

    Change template rendering from index.html to home.html

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ application=Flask(__name__)
 app=application
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
-        return render_template('index.html')
+        return render_template('home.html')
     else:
         data=CustomData(
             gender=request.form.get('gender'),
@@ -28,6 +28,6 @@ def predict_datapoint():
         print(pred_df)
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
-        return render_template('index.html',results=results[0])
+        return render_template('home.html',results=results[0])
 if __name__=="__main__":
     app.run(host="0.0.0.0")
