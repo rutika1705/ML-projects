3a666eb401245ac705b8c49236cf62e0a03d47a8
rutika1705 <rutikatharali@gmail.com>
2026-03-07 11:14:55 +0530

    Change rendered template from home.html to index.html

--- app.py
+++ app.py
@@ -28,6 +28,6 @@ def predict_datapoint():
         print(pred_df)
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
+        return render_template('index.html',results=results[0])
 if __name__=="__main__":
     app.run(host="0.0.0.0")
